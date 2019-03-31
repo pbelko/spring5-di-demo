e3d52192e78699ab8b5ba7e849bfeeadca1cb168
Pat Belko <belko43@students.rowan.edu>
2019-03-31 12:20:09 -0400

    Added capability for using environment properties

--- src/main/java/guru/springframework/config/PropertyConf.java
+++ src/main/java/guru/springframework/config/PropertyConf.java
@@ -1,15 +1,21 @@
 package guru.springframework.config;
 
 import guru.springframework.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class PropertyConf {
+
+    @Autowired
+    Environment env;
+
     @Value("${guru.username}")
     String user;
 
@@ -22,7 +28,7 @@ public class PropertyConf {
     @Bean
     public FakeDataSource fakeDataSource(){
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUser(user);
+        fakeDataSource.setUser(env.getProperty("USERNAME"));
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(url);
         return fakeDataSource;
